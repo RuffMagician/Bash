7ad8c4ae91b09496c6d954499c1d2c9303ab7998
RuffMagician <navarrito96@gmail.com>
2017-06-28 20:22:47 -0700

    Update myName.sh

--- myName.sh
+++ myName.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 
-echo "Enter a Number: "
-read myNum
+myName=""
+while [[ ! $myName =~ ^[A-Za-z]+$ ]]
+do
+	read -p "Please Enter Your Name: " myName
+	if [[ ! ${myName} -ge 2 ]] 
+	then
+		read -p "Please Keep your name over 2 Characters: " myName
+	elif [[ ! ${myName} -le 25 ]] && unset myName
+	then
+		read -p "Please Keep your name under 25 Characters: " myName
+	fi
+done
 
-numLen=`echo $myNum | wc -c`
-if [ $myNum -le 10 ]
-then
-	echo "$myNum is Less then 10"
-else
-	echo "$myNum is More than 10"
-fi
+echo "Hello $myName!"
 
-echo "Length of the string is " $myNum
+myNum=""
+
+# Checking if it is a number
+while [[ ! $myNum =~ ^[0-9]+$ ]]
+do
+	# If it is not a number or does not fall within the range it will display this
+	read -p "Please Enter a Number Between 5 - 100: " myNum
+
+	# unset resets the variable to re input a new number in the variable
+	! [[ ${myNum} -ge 5 && ${myNum} -le 100  ]] && unset myNum
+
+	# Check if number is even
+	while [[ ! $((myNum%2)) -eq 0 ]]; do
+		unset myNum
+		read -p "Please enter an Even Number: " myNum
+	done
+done
+# Count by 2
+for ((i=2; i<=myNum; i+=2)); do
+    echo $i
+done
+
+echo "Congratulations your number is $myNum!"
